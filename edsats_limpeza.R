d159f4b75cb40e2d27cff8bc253ea0f1b8b7bfde
Leticia Souza <leticia.suely@ufpe.br>
2021-04-25 20:32:01 -0300

    Códigos e comentários

--- edsats_limpeza.R
+++ edsats_limpeza.R
@@ -11,6 +11,7 @@ skimr::skim(edstats)
 edstats_filtrado <- edstats %>% 
   select(-c( "X2020", "X2025", "X2030", "X2035", "X2040", "X2045", "X2050", "X2055", "X2060", "X2065", "X2070", "X2075", "X2080", "X2085", "X2090", "X2095", "X2100", "X"))
 
+
 # Mudando o formato
 
 edstats_mod <- edstats_filtrado %>% 
@@ -22,7 +23,9 @@ edstats_mod <- edstats_filtrado %>%
 
 glimpse(edstats_mod)
 
+
 # selecionando os indicadores
+
 library(readxl)
 Variaveis_edstats <- read_excel("Variaveis_edstats.xlsx") %>% 
   select(Código) %>% 
@@ -33,16 +36,19 @@ edstats_mod <- edstats_mod %>%
   filter(Indicator.Code %in% Variaveis_edstats$Código)
 
 # tirando valores de países agregados
+
 agregados <- c("WLD", "ARB", "EAP","EAS", "ECA","ECS", "EMU", "EUU", "HPC", "LCN", "LDC", "LIC", "LMC", "LMY", "MEA", "MIC", "MNA", "NAC", "OED", "SAS", "SSA", "SSF", "UMC", "HIC", "LAC")
 
 edstats_mod <- edstats_mod %>% 
   filter(!Country.Code %in% agregados) %>% 
   mutate(year = substring(year, 2))
 
-# Juntando bancos por codigo de 3 letras dos países
+##########################################
+
+# juntando bases pelo nome do país
 
 banco_joined <- left_join(edstats_mod, pv2018_mod1, 
-                          by = c("Country.Code" = "scode",
+                          by = c("ï..Country.Name" = "country",
                                  "year" = "year"))
 View(banco_joined)
 
